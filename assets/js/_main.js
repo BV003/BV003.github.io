36adb91221417c345d7cccca104b6b0c5a2ab9f0
Michael Liu <bbz975749@gmail.com>
2025-08-21 15:17:24 +0800

    改浅色

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -33,22 +33,22 @@ let setTheme = (theme) => {
   //   browserPref;
   const use_theme = 'light'; 
 
-  if (use_theme === "dark") {
-    $("html").attr("data-theme", "dark");
-    $("#theme-icon").removeClass("fa-sun").addClass("fa-moon");
-  } else if (use_theme === "light") {
-    $("html").removeAttr("data-theme");
-    $("#theme-icon").removeClass("fa-moon").addClass("fa-sun");
-  }
+  // if (use_theme === "dark") {
+  //   $("html").attr("data-theme", "dark");
+  //   $("#theme-icon").removeClass("fa-sun").addClass("fa-moon");
+  // } else if (use_theme === "light") {
+  //   $("html").removeAttr("data-theme");
+  //   $("#theme-icon").removeClass("fa-moon").addClass("fa-sun");
+  // }
 };
 
 // Toggle the theme manually
-var toggleTheme = () => {
-  const current_theme = $("html").attr("data-theme");
-  const new_theme = current_theme === "dark" ? "light" : "dark";
-  localStorage.setItem("theme", new_theme);
-  setTheme(new_theme);
-};
+// var toggleTheme = () => {
+//   const current_theme = $("html").attr("data-theme");
+//   const new_theme = current_theme === "dark" ? "light" : "dark";
+//   localStorage.setItem("theme", new_theme);
+//   setTheme(new_theme);
+// };
 
 /* ==========================================================================
    Plotly integration script so that Markdown codeblocks will be rendered
